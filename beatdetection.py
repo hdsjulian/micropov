14863d351ca6c3348a7855a9ae9ce7f1362c7465
Julian Finn <julian@phinn.de>
2019-03-03 23:31:32 +0100

    this doesn't work but why?

--- beatdetection.py
+++ beatdetection.py
@@ -1,5 +1,7 @@
 import machine, neopixel, esp, network, time, math, utime
 from generic_dotstar import Apa102DotStar as APA102
+#from apa102 import APA102
+from ringbuffer import RINGBUFFER
 ### MICROPHONE
 OUT = 35  # Connect it to OUT on microphone
 NUM_LEDS = 60
@@ -9,26 +11,21 @@ clock = machine.Pin(18)
 data = machine.Pin(23)
 apa = APA102(clock, data, NUM_LEDS)
 timer = machine.Timer(0)
-
-mid = 2048
-envelope = 0
-threshold = 1500
-beatline = 100
+rbuffer = RINGBUFFER(100)
 
 def handleInterrupt(timer):
-	global envelope, mid, beatline
-	val = mic.read()
-	if (val > mid): mid +=1 
-	if (val < mid): mid -=1 
-	envelope = abs(mid-val)
-	if envelope > beatline: 
-		beatline = envelope
-	else: 
-		beatline -= 5
+    global rbuffer, mic
+    rbuffer.put(mic.read())
+
 
 timer.init(period=1, mode=machine.Timer.PERIODIC, callback=handleInterrupt)
 
+
 def run(threshold):
+    mid = 2048
+    envelope = 0
+    threshold = 1500
+    beatline = 100
     first_zero = 1
     highest = 50
     brightness = 3
@@ -39,12 +36,24 @@ def run(threshold):
     offbeat = True
     offbeatcounter = 0
     offbeatcountme = 0
+
     while True:
-    	global beatline
+        global rbuffer
+        val = rbuffer.get()
+        if val != None: 
+            if (val > mid): mid +=1 
+            if (val < mid): mid -=1 
+            envelope = abs(mid-val)
+            if envelope > beatline: 
+                beatline = envelope
+            else: 
+                beatline -= 5
+        else: 
+            continue
     	env = beatline
     	if counter < 5000:
     	    if counter % 100 == 0:
-    		    print (counter)
+    		    print (beatline)
     	    counter +=1 
     	    continue
     	if env > threshold and beat == False:
@@ -52,6 +61,7 @@ def run(threshold):
     	    beat = True
     	    beatchange = True
     	    beatcounter +=1
+            brightness = 20
     	    print('beat no'+str(beatcounter)+" Envelope Value: "+str(env)+" Threshold: "+str(threshold))
     	    print ("Number of reads below threshold during offbeat "+str(offbeatcountme))
     	    offbeatcountme = 0
@@ -60,6 +70,7 @@ def run(threshold):
     	    print ("Same Beat  Envelope Value: "+str(env)+"   Threshold "+str(threshold))
     	elif env < threshold and offbeat == False:
     	    beat = False
+            brightness = 3
     	    offbeatcounter += 1
     	    offbeat = True
     	    beatchange = True
@@ -67,5 +78,8 @@ def run(threshold):
     	elif env < threshold and offbeat == True: 
     	    beatchange = False
     	    offbeatcountme +=1
-        time.sleep(0.001)
+        if beatchange == True: 
+            for i in range(NUM_LEDS):
+                apa[i] = (255, 0, 0, brightness)
+            apa.write()
 run(1000)
