a2b3bea5938a32fe7a07953b8865c12c005ac8c4
Julian Finn <julian@phinn.de>
2019-03-03 22:48:48 +0100

    zwischenspeichern

--- beatdetection.py
+++ beatdetection.py
@@ -12,50 +12,60 @@ timer = machine.Timer(0)
 
 mid = 2048
 envelope = 0
+threshold = 1500
+beatline = 100
 
 def handleInterrupt(timer):
-	global envelope, mid
+	global envelope, mid, beatline
 	val = mic.read()
 	if (val > mid): mid +=1 
 	if (val < mid): mid -=1 
 	envelope = abs(mid-val)
+	if envelope > beatline: 
+		beatline = envelope
+	else: 
+		beatline -= 5
+
 timer.init(period=1, mode=machine.Timer.PERIODIC, callback=handleInterrupt)
 
 def run(threshold):
-    global ringbuffer
     first_zero = 1
     highest = 50
     brightness = 3
     beatcounter = 0
-    offbeatcounter = 0
     counter = 0
     beat = False
     beatchange = False
     offbeat = True
+    offbeatcounter = 0
+    offbeatcountme = 0
     while True:
-    	global envelope
-    	print(envelope)
+    	global beatline
+    	env = beatline
     	if counter < 5000:
     	    if counter % 100 == 0:
     		    print (counter)
     	    counter +=1 
     	    continue
-    	if envelope > threshold and beat == False:
+    	if env > threshold and beat == False:
     	    offbeat = False
     	    beat = True
     	    beatchange = True
     	    beatcounter +=1
-    	    print('beat no'+str(beatcounter)+" envelope "+str(envelope))
+    	    print('beat no'+str(beatcounter)+" Envelope Value: "+str(env)+" Threshold: "+str(threshold))
+    	    print ("Number of reads below threshold during offbeat "+str(offbeatcountme))
+    	    offbeatcountme = 0
     	elif envelope > threshold and beat == True: 
     	    beatchange == False
-    	    print ("Same Beat"+str(envelope)+"   "+str(counter))
-    	elif envelope < threshold and offbeat == False:
+    	    print ("Same Beat  Envelope Value: "+str(env)+"   Threshold "+str(threshold))
+    	elif env < threshold and offbeat == False:
     	    beat = False
     	    offbeatcounter += 1
     	    offbeat = True
     	    beatchange = True
-    	    print ("Offbeat " + str(offbeat)+ " envelope "+str(envelope))
-    	elif envelope < threshold and offbeat == True: 
+    	    print ("Offbeat " + str(offbeatcounter)+ " Envelope Value "+str(env))
+    	elif env < threshold and offbeat == True: 
     	    beatchange = False
+    	    offbeatcountme +=1
         time.sleep(0.001)
-run(500)
+run(1000)
